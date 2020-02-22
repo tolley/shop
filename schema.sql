b3f867f0a98fd0f59d87e209beff137ebb47a1af
tolley <chris.tolley@gmail.com>
2020-02-22 01:48:33 -0500

    Started the sql schema

---
+++ schema.sql
@@ -0,0 +1,23 @@
+# A table to hold the product types
+create table product_types (
+	id int(11) unsigned not null auto_increment primary key,
+	name varchar(255) not null unique,
+	description text,
+	prettyName varchar(255),
+	default_thumbnail varchar(255) comment "This image is used when the individual product has no image",
+	created_at timestamp,
+	updated_at timestamp
+);
+
+# Holds the data for each product. This is not that individual instances of a product
+create table product (
+	id int(11) unsigned not null auto_increment primary key,
+	name varchar(255) not null,
+	description text,
+	type int(11) unsigned not null,
+	thumbnail varchar(255) default "",
+	active tinyint(1) unsigned not null default 0,
+	created_at timestamp,
+	updated_at timestamp
+);
+
